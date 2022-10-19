3789374db789297084822ce593885169f9e7f63c
CORNELIUS APAALSE <cornelius.apaalse@ashesi.edu.gh>
2022-10-19 23:03:47 +0000

    cred update

--- settings/db_cred.php
+++ settings/db_cred.php
@@ -1,14 +1,14 @@
 <?php
 //Database credentials
-// define ('SERVER', 'us-cdbr-east-06.cleardb.net');
-// define ('PASSWORD','35419c79');
-// define ('DATABASE','heroku_ca7fe59d816b178');
-// define ('USERNAME','b6f0480060d040');
+define ('SERVER', 'us-cdbr-east-06.cleardb.net');
+define ('PASSWORD','35419c79');
+define ('DATABASE','heroku_ca7fe59d816b178');
+define ('USERNAME','b6f0480060d040');
 
-define ("SERVER", "localhost");
-define ("PASSWORD","");
-define ("DATABASE","shoppn");
-define ("USERNAME","root");
+// define ("SERVER", "localhost");
+// define ("PASSWORD","");
+// define ("DATABASE","shoppn");
+// define ("USERNAME","root");
 
 
 
