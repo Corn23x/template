5c608b839339382ec18385f687e46efa142e286d
CORNELIUS APAALSE <cornelius.apaalse@ashesi.edu.gh>
2022-11-12 17:28:02 +0000

    cart management

--- actions/manage_quantity_cart.php
+++ actions/manage_quantity_cart.php
@@ -3,23 +3,6 @@ include_once(dirname(__DIR__, 1) . '/controllers/cart_controller.php');
 include_once(dirname(__DIR__, 1) . '/controllers/product_controller.php');
 include_once(dirname(__DIR__, 1) . '/settings/core.php');
 
-// if (isset($_GET['updatequant'])) {
-
-//     $id = $_SESSION['id'];
-//     $productid= $_GET['updatequant'];
-
-//     $quantity = $_GET['updatequant'];
-
-//     $result = update_quant($id, $quantity, $productid);
-
-//     if ($result) {
-//         echo "<script>alert('Quantity has been updated')</script>";
-//         echo "<script>window.open('../view/cart.php','_self')</script>";
-//     } else {
-//         echo "<script>alert('Quantity has not been updated')</script>";
-//         echo "<script>window.open('../view/cart.php','_self')</script>";
-//     }
-// }
 
 if(isset($_GET['updatequant'])){
 
@@ -31,7 +14,7 @@ if(isset($_GET['updatequant'])){
     $quantity = $_GET['updatequant'];
     
     $result = update_quant($user_id, $quantity, $product_id);
-    
+
     if ($result) {
                 echo "<script>alert('Quantity has been updated')</script>";
                 echo "<script>window.open('../view/cart.php','_self')</script>";
